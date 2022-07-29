13a0bbf58ac11dc59e4eb704a00fdf6e02b3b9d4
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-07-29 13:40:46 +0200

    rough layout

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {APP_BASE_HREF} from '@angular/common';
+import {ConcurrencyRelation, DropFile, FD_CONCURRENCY, FD_LOG, FD_PETRI_NET, Trace, XesLogParserService} from 'ilpn-components';
+import {FormControl} from '@angular/forms';
 
 @Component({
     selector: 'app-root',
@@ -11,4 +13,30 @@ import {APP_BASE_HREF} from '@angular/common';
     ]
 })
 export class AppComponent {
+
+    public FD_LOG = FD_LOG;
+    public FD_CONCURRENCY = FD_CONCURRENCY;
+    public FD_PN = FD_PETRI_NET;
+
+    public log: Array<Trace> | undefined = undefined;
+    public concurrency: ConcurrencyRelation | undefined = undefined;
+
+    public result: Array<DropFile> | undefined = undefined;
+
+    public fcAddStartStop: FormControl;
+    public fcRemovePrefixes: FormControl;
+
+
+    constructor(private _logParser: XesLogParserService) {
+        this.fcAddStartStop = new FormControl(false);
+        this.fcRemovePrefixes = new FormControl(false);
+    }
+
+    processLogUpload(files: Array<DropFile>) {
+
+    }
+
+    processConcurrencyUpload(files: Array<DropFile>) {
+
+    }
 }
